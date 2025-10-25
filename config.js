a3749aaf253e2de88b19e9a007e2d56a145977ad
senuff1234 <akmaxmy2@gmail.com>
2025-10-26 01:13:24 +0530

    Update config.js

--- config.js
+++ config.js
@@ -6,7 +6,7 @@ function convertToBool(text, fault = 'true') {
 }
 module.exports = {
 SESSION_ID: process.env.SESSION_ID || "0dkkwRSa#Z3ztefU_Z2zFQ2Up9LgOpUX9E_KMkMhYquJSPBOTRYQ",
-ALIVE_IMG: process.env.ALIVE_IMG || "https://github.com/senuff1234/Senuwa-bot/blob/main/images/Senuwa-md.png?raw=true",
+ALIVE_IMG: process.env.ALIVE_IMG || "https://github.com/senuff1234/Senuwa-bot/blob/main/images/Senuwa%20md.jpg?raw=true",
 ALIVE_MSG: process.env.ALIVE_MSG || "*Hello👋 SENUWA-MD Is Alive Now😍*",
 BOT_OWNER: '94704206946',  // Replace with the owner's phone number
 
